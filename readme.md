e5dc71faedaa86b90abcaed3d4ef80111f5ac23d
Shigma <1700011071@pku.edu.cn>
2022-02-25 04:00:44 +0800

    add implementation

--- readme.md
+++ readme.md
@@ -2,4 +2,10 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-oeis?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-oeis)
 
-Use [OEIS](https://oeis.org/) in Koishi
+Use [OEIS](https://oeis.org/) in Koishi. OEIS stands for the **O**n-line **E**ncyclopedia of **I**nteger **S**equences.
+
+## 指令：oeis
+
+输入用逗号隔开的数作为要查询的数列的前几项，或者直接输入以 id:A 打头的数列编号。
+
+- 基本语法：`oeis <sequence>`
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-import { Context } from 'koishi'
+import { Context, Schema } from 'koishi'
 
-export const name = 'oeis'
+const BASE_URL = 'https://oeis.org'
+
+export interface Config {}
+
+export const name = 'OEIS'
+export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command('oeis <sequence>', '数列查询')
+    .option('start', '-s <start>  设置起始页码', { fallback: 0 })
+    .usage('输入用逗号隔开的数作为要查询的数列的前几项，或者直接输入以 id:A 打头的数列编号。')
+    .example('oeis 1,2,3,6,11,23,47,106,235')
+    .example('oeis id:A000055')
+    .action(async ({ options, session }, sequence) => {
+      const data = await ctx.http.get(`${BASE_URL}/search?fmt=json&q=${sequence}&start=${options.start}`)
+      for (const result of data.results) {
+        if (result.name.startsWith('Duplicate')) continue
+        session.send([
+          `${BASE_URL}/A${String(result.number).padStart(6, '0')}`,
+          `${result.name}${result.id ? ` (${result.id})` : ''}`,
+          result.data,
+          ...result.comment,
+        ].join('\n'))
+      }
+    })
 }
